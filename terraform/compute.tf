11bd2cf958060b9103e671d0d41c911bc4d5e36d
Robin Smorenburg <robin@smorenburg.io>
2022-01-26 11:55:55 +0100

    Remove google-monitoring-enabled

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -161,7 +161,6 @@ resource "google_compute_instance_template" "ghost" {
     block-project-ssh-keys    = true
     enable-oslogin            = true
     google-logging-enabled    = true
-    google-monitoring-enabled = true
     user-data                 = local.cloud_config
   }
 
